9db655e6fca7038155ce60b5827572aa6d88a71a
Roderik van der Veer <roderik@thor.kunstmaan.be>
2012-12-07 17:06:50 +0100

    Added the bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+if rvm --version 2>/dev/null; then
+  gem install soloist
+else
+  sudo gem install soloist
+fi
+
+mkdir -p ~/cookbooks; cd ~/cookbooks
+cat > soloistrc <<EOF
+cookbook_paths:
+- $PWD
+recipes:
+- pivotal_workstation::create_var_chef_cache
+- pivotal_workstation::xquartz
+- pivotal_workstation::locate_on
+- pivotal_workstation::1password
+- pivotal_workstation::bash4
+- pivotal_workstation::bash_completion
+- pivotal_workstation::coreutils
+- pivotal_workstation::findutils
+- pivotal_workstation::wget
+- pivotal_workstation::pwgen
+- pivotal_workstation::siege
+- pivotal_workstation::xmlstarlet
+- pivotal_workstation::watch
+- pivotal_workstation::gem_no_rdoc_no_ri
+- pivotal_workstation::rvm
+- pivotal_workstation::alfred
+- pivotal_workstation::chrome
+- pivotal_workstation::dropbox
+- pivotal_workstation::firefox
+- pivotal_workstation::github_for_mac
+- pivotal_workstation::bartender
+- pivotal_workstation::istatmenus
+- pivotal_workstation::phpstorm
+- pivotal_workstation::things
+- pivotal_workstation::viscosity
+- pivotal_workstation::root_shell_bash
+- pivotal_workstation::optipng
+- pivotal_workstation::jpegoptim
+- pivotal_workstation::gnu-tar
+- pivotal_workstation::mysql
+- pivotal_workstation::postgres
+- pivotal_workstation::psycopg2
+- pivotal_workstation::mysql-python
+- pivotal_workstation::imagemagick
+- pivotal_workstation::unhide_home
+- pivotal_workstation::php54
+- pivotal_workstation::mod-jk
+- pivotal_workstation::composer
+- roderik_workstation::sublime_packages
+- roderik_workstation::bashrc
+- roderik_workstation::inputrc
+- kmachine::update
+- kmachine::ssh_config
+- kmachine::kms
+- kmachine::kstrano
+- kmachine::vagrant
+- kmachine::kdeploy
+- kmachine::apache_kdeploy
+- kmachine::phpconfig
+EOF
+if [[ -d pivotal_workstation ]]; then
+  cd pivotal_workstation && git pull && cd ..
+else
+  git clone https://github.com/roderik/pivotal_workstation.git
+fi
+if [[ -d dmg ]]; then
+  cd dmg && git pull && cd ..
+else
+  git clone https://github.com/opscode-cookbooks/dmg.git
+fi
+soloist
