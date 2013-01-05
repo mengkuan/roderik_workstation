011b19e3438d80dfbfe94ed6aba9fc62e7df3bd1
Roderik van der Veer <roderik@vanderveer.be>
2013-01-05 22:54:35 +0100

    Move some files around

--- attributes/bash_it.rb
+++ attributes/bash_it.rb
@@ -4,13 +4,15 @@ node.override['bash_it'] ={
     'completion' => %w[defaults git brew gem rake ssh],
     'plugins'    => %w[base git rvm ssh vagrant ]
   },
-  'custom_plugins' => %w[
-    bash_it/custom/ensure_usr_local_bin_first.bash
-    bash_it/custom/aliases.bash
-    bash_it/custom/base.bash
-    bash_it/custom/exports.bash
-    bash_it/custom/functions.bash
-  ],
+  'custom_plugins' => {
+      "pivotal_workstation" => %w[bash_it/custom/ensure_usr_local_bin_first.bash],
+      "roderik_workstation" => %w[
+        bash_it/custom/aliases.bash
+        bash_it/custom/base.bash
+        bash_it/custom/exports.bash
+        bash_it/custom/functions.bash
+      ]
+  },
   'theme' => 'roderik',
   'dir' => '/etc/bash_it',
   'repository' => 'https://github.com/roderik/bash-it.git'
--- attributes/bash_profile_include.rb
+++
@@ -1,6 +0,0 @@
-node.override["bash_includes_subdir"]="/etc/bash.d"
-if node["platform"] == "ubuntu"
-    node.override["bash_bashrc"]="/etc/bash.bashrc"
-else
-    node.override["bash_bashrc"]="/etc/bashrc"
-end
